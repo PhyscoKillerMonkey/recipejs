acee49bb060921d4ef87a2152bbbc1320ff21935
Reece Bennett <reece.bennett1@gmail.com>
2017-09-25 22:56:51 +0100

    Add MongoDB to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // app.js
 
 const express = require("express");
-const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const db = require("./config/db");
+
+const app = express();
 const port = 8080;
 
 // View engine setup
@@ -10,12 +14,23 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Middleware setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define routes
-app.use("/", require("./routes/index"));
+// Connect to the database server
+MongoClient.connect(db.url, (err, database) => {
+  if (err) {
+    console.error("Database connection failed: " + err);
+    return;
+  }
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  // Define routes
+  app.use("/", require("./routes/index"));
+  app.use("/recipes", require("./routes/recipes/index.js")(database));
+
+  // Database connected, start the server now
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 });
