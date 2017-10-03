9bc0f3f01baf5f6ef2d6390f92cda34cf9e2e168
Reece Bennett <reece.bennett1@gmail.com>
2017-10-03 22:34:43 +0100

    Add method to form and ingredients page

--- routes/new.js
+++ routes/new.js
@@ -9,18 +9,35 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    const recipe = { "ingredients": [] };
+    const recipe = {
+      "ingredients": [],
+      "method": []
+    };
 
     ["title", "author", "time", "description"].forEach((field) => {
       recipe[field] = req.body[field];
     });
-    
-    for (var i = 0; i < req.body["ingredient"].length; i++) {
-      recipe["ingredients"].push({
-        "ingredient": req.body["ingredient"][i],
-        "amount": req.body["amount"][i]
-      });
+
+    if (typeof(req.body.ingredient) === "string") {
+      recipe.ingredients = [{
+        "ingredient": req.body.ingredient,
+        "amount": req.body.amount
+      }];
+    } else {
+      for (var i = 0; i < req.body.ingredient.length; i++) {
+        recipe.ingredients.push({
+          "ingredient": req.body.ingredient[i],
+          "amount": req.body.amount[i]
+        });
+      }
     }
+
+    if (typeof(req.body.method) === "string") {
+      recipe.method = [req.body.method];
+    } else {
+      recipe.method = req.body.method;
+    }
+    
 
     db.createRecipe(recipe, (err, result) => {
       console.log(err, result.ops[0]);
